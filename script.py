8d28f0168815490ad956031025bbbd7f9830e6b9
Rafael Alexander Riatiga Duran <rafael.riatiga@udea.edu.co>
2026-05-16 14:47:07 -0500

    Configuracion de credenciales locales de MySQL y creacion del motor de conexion base

--- script.py
+++ script.py
@@ -1,14 +1,31 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 import sys
 
 # Cargar las variables ocultas del archivo .env
 load_dotenv()
 
+# Extraer los valores y guardarlos en variables de Python
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Crear una URL genérica (SIN el nombre de la base de datos al final)
+URL_SERVIDOR = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}"
+
+# Crear la URL completa que usará SQLAlchemy después
+DATABASE_URL = f"{URL_SERVIDOR}/{DB_NAME}"
+
+# Crear el motor de conexión inicial apuntando al servidor general
+engine = create_engine(URL_SERVIDOR)
+
 # Función principal para ejecutar el proceso completo
 def main():
     try:
-        print("Iniciando la configuracion de la automatizacion...")
+        print("Variables de entorno cargadas y motor de conexion inicial creado con exito.")
     except Exception as e:
         print(f"\n[ERROR CRÍTICO]: El proceso se detuvo debido a: {e}", file=sys.stderr)
 
